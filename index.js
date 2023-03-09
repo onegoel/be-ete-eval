39bc3e5db03e66df28cd369ef4385d2ef01ffc96
Aryan Goel <aryangoel6900@yahoo.in>
2023-03-09 13:46:00 +0530

    chore(be-server): initial server set up with tester route

---
+++ index.js
@@ -0,0 +1,23 @@
+require('dotenv').config();
+const express = require('express');
+const app = express();
+const port = process.env.SERVER_PORT;
+const { contentRouter, collectionRouter } = require('./src/routes');
+//const db = require('./src/models');
+const cors = require('cors');
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.text());
+app.use(cors({
+    origin: ['http://localhost:3000', 'http://localhost:8100'],
+    credentials: true
+}));
+app.use('/api/content', contentRouter);
+app.use('api/collection', collectionRouter);
+
+// db.sequelize.sync({ force: false }).then(() => {
+app.listen(port, () => {
+    console.log(`Backend server running at http://${process.env.SERVER_HOST}:${port}`);
+});
+// });
---
+++ src/routes/index.js
@@ -0,0 +1,7 @@
+const contentRouter = require('./content.routes');
+const collectionRouter = require('./collection.routes');
+
+module.exports = {
+    contentRouter,
+    collectionRouter
+};
